4b922c18ab50599c842e8f6fb20e32d3618457d7
Dr Ian Preston <ianopolous@protonmail.com>
2019-08-01 22:01:47 +0100

    Log errors and use our test stripe key

--- src/peergos/payments/http/CardHandler.java
+++ src/peergos/payments/http/CardHandler.java
@@ -4,33 +4,40 @@ import com.sun.net.httpserver.*;
 
 import java.io.*;
 import java.util.*;
+import java.util.logging.*;
 
 public class CardHandler implements HttpHandler {
 
+    private static final Logger LOG = Logger.getLogger("NULL_FORMAT");
     private static final int MAX_PAYLOAD_SIZE = 4096;
 
     public CardHandler() {}
 
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
+    public void handle(HttpExchange exchange) {
+        try {
+            byte[] bodyBytes = readFully(exchange.getRequestBody(), MAX_PAYLOAD_SIZE);
+            String body = new String(bodyBytes);
 
-        byte[] bodyBytes = readFully(exchange.getRequestBody(), MAX_PAYLOAD_SIZE);
-        String body = new String(bodyBytes);
+            String[] parts = body.split("&");
+            Map<String, String> params = new HashMap<>();
+            for (String part : parts) {
+                String[] mapping = part.split("=");
+                String value = mapping.length > 1 ? mapping[1] : "";
+                params.put(mapping[0], value);
+            }
+            System.out.println(params);
 
-        String[] parts = body.split("&");
-        Map<String, String> params = new HashMap<>();
-        for (String part : parts) {
-            String[] mapping = part.split("=");
-            params.put(mapping[0], mapping[1]);
+            // TODO store in database
+
+            byte[] resp = "<html><body><h1>Card accepted</h1></body></html>".getBytes();
+            exchange.sendResponseHeaders(200, resp.length);
+            exchange.getResponseBody().write(resp);
+            exchange.close();
+        } catch (Exception e) {
+            LOG.log(Level.SEVERE, e, e::getMessage);
+            throw new RuntimeException(e);
         }
-        System.out.println(params);
-
-        // TODO store in database
-
-        byte[] resp = "<html><body><h1>Card accepted</h1></body></html>".getBytes();
-        exchange.sendResponseHeaders(200, resp.length);
-        exchange.getResponseBody().write(resp);
-        exchange.close();
     }
 
     public static byte[] readFully(InputStream in, int maxSize) throws IOException {
