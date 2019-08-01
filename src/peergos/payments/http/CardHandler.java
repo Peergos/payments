fc1e769a402722b8a5781b9f718d7742c965b644
Dr Ian Preston <ianopolous@protonmail.com>
2019-08-01 20:46:11 +0100

    Add extra parameters to the form submission and parse them in handler

--- src/peergos/payments/http/CardHandler.java
+++ src/peergos/payments/http/CardHandler.java
@@ -3,6 +3,7 @@ package peergos.payments.http;
 import com.sun.net.httpserver.*;
 
 import java.io.*;
+import java.util.*;
 
 public class CardHandler implements HttpHandler {
 
@@ -15,7 +16,17 @@ public class CardHandler implements HttpHandler {
 
         byte[] bodyBytes = readFully(exchange.getRequestBody(), MAX_PAYLOAD_SIZE);
         String body = new String(bodyBytes);
-        System.out.println(body);
+
+        String[] parts = body.split("&");
+        Map<String, String> params = new HashMap<>();
+        for (String part : parts) {
+            String[] mapping = part.split("=");
+            params.put(mapping[0], mapping[1]);
+        }
+        System.out.println(params);
+
+        // TODO store in database
+
         byte[] resp = "<html><body><h1>Card accepted</h1></body></html>".getBytes();
         exchange.sendResponseHeaders(200, resp.length);
         exchange.getResponseBody().write(resp);
@@ -26,8 +37,11 @@ public class CardHandler implements HttpHandler {
         ByteArrayOutputStream bout =  new ByteArrayOutputStream();
         byte[] b =  new  byte[0x1000];
         int nRead;
-        while ((nRead = in.read(b, 0, b.length)) != -1 )
+        while ((nRead = in.read(b, 0, b.length)) != -1 ) {
             bout.write(b, 0, nRead);
+            if (bout.size() > maxSize)
+                throw new IllegalStateException("Too much data to read!");
+        }
         in.close();
         return bout.toByteArray();
     }
