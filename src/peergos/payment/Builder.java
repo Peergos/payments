0395329745875c688327b505f4c72a37b9337108
ian <ianopolous@protonmail.com>
2021-02-23 01:22:03 +0000

    Fix setting free quotas

--- src/peergos/payment/Builder.java
+++ src/peergos/payment/Builder.java
@@ -71,7 +71,7 @@ public class Builder {
 
     protected static Pricer buildPricer(Args a) {
         boolean fixedPrices = a.hasArg("quota-prices");
-        if (a.hasArg("allowed-quotas"))
+        if (!fixedPrices && a.hasArg("allowed-quotas"))
             throw new IllegalStateException("Allowed quotas are listed, but not prices! (quota-prices arg)");
         if (! fixedPrices)
             return new LinearPricer(new Natural(1024 * 1024 * 1024L / 50));
